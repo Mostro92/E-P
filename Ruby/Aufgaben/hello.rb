655331ccb4a69980f26364efa7227a552b3ea0a6
Mostro92 <m-ostrowitzki@t-online.de>
2016-09-20 10:21:49 +0200

    new

---
+++ Ruby/Aufgaben/hello.rb
@@ -0,0 +1,9 @@
+hello = "Hallo Marten wie geht es dir?"
+time = Time.new
+3.times do
+	puts hello
+end	
+1.times do
+	puts time
+end
+
---
+++ Ruby/Aufgaben/start.rb
@@ -0,0 +1,9 @@
+var = "Mikey loves Goonies"
+
+puts 'Mickey\'s favourite movie is Goonies'
+
+movie = "Goonies"
+#String ersätzung \n Zeilen umbruch \' simikolen
+puts "Mickey\'s favourite Movie is \n" + movie
+rank = 10
+puts "#{movie} has a rank of #{rank * 2} am #{Time.now}"
