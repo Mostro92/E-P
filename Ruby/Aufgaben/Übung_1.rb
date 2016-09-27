256be8ea4738a1ef1d5f82938cd8bec6a57a985a
Mostro92 <m-ostrowitzki@t-online.de>
2016-09-27 12:49:44 +0200

    new

--- Ruby/Aufgaben/Übung_1.rb
+++ Ruby/Aufgaben/Übung_1.rb
@@ -12,12 +12,23 @@ puts movie_listing(ARGV[0], ARGV[1])
 #Klassen
 
 class Movie
+	
+	attr_reader :title
+	attr_writer :title
+	attr_accessor :title
 	def initialize(pname, prank=5)
 		@name = pname
 		@rank = prank
 
 
 	end
+	def normalized_rank
+		@rank * 1000
+	#Getter für einen veränderten rank 
+
+	def titel=(titel)
+		@title = title
+	end	
 
 	def list_movie
 		"Movie heisst:#{@name} und hat einen rank von: #{@rank}"
@@ -42,4 +53,6 @@ puts obj.list_movie
 obj2 = Movie.new("Batman", 10)
 puts obj2.list_movie
 
+puts movie.titel
+puts movie
 #-----------------------------------------------------------------#
