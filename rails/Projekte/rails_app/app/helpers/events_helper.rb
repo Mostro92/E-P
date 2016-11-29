040176dddd11a483731c7cf7ede0cafbdd299e75
Mostro92 <m-ostrowitzki@t-online.de>
2016-11-29 12:54:34 +0100

    rails

--- rails/Projekte/rails_app/app/helpers/events_helper.rb
+++ rails/Projekte/rails_app/app/helpers/events_helper.rb
@@ -2,7 +2,8 @@ module EventsHelper
 
 	def format_price(event)
 		if event.price.blank? || event.price.zero?
-			"<strong>Free</strong>".html_safe
+			#<strong>Free</strong>".html_safe
+			content_tag(:strong, 'Free')
 		else
 			number_to_currency event.price 
 		end	
